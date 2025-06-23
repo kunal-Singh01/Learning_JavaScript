6d382bb0e1d60314a0f119701b8eff8088767f04
kunal <kunaljatnic@gmail.com>
2025-06-23 23:18:03 +0530

    Basics

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as a newer version
+
+console.log(3+3)
+console.log("Kunal")
+
+
+let name="kunal"
+let age=20
+let isLoggedIn=false
+
+
+//Types of DataTypes
+
+//numbers
+//bigint
+//string=""
+//boolean=true/false
+//null=standalone value
+//undefined
+//symbol=unique
+
+console.log(typeof undefined) // => undefined
+console.log(typeof null) // => object
