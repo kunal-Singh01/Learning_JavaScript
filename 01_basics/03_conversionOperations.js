ef44f52173a9e3da5cdb9942bb031a8fd56bcae3
kunal <kunaljatnic@gmail.com>
2025-07-02 19:02:57 +0530

    LEARNING

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,24 @@
+let score=true
+
+console.log(typeof score) 
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//33abc => NaN
+//"33" => 33
+//"" => 0
+//1 => true,0 => false
+
+
+
+// let IsLogged=""
+
+// let BooleanLoggedIn=Boolean(IsLogged)
+// console.log(BooleanLoggedIn);
+// console.log(typeof BooleanLoggedIn); 
+
+//1 => true,0 => false
+//"" => false
+//"Kunal" => true
